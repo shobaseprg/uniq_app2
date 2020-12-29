db5b3ec6b837ec49d2c6592409ad50b448b6f71b
shobaseprg <pgymlsho19128@gmail.com>
2020-12-30 00:40:00 +0900

    mongoose test

--- src/test_db.js
+++ src/test_db.js
@@ -1,43 +1,17 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+//mongooseをロード
+const mongoose = require("mongoose");
+//データーベース接続を設定
+mongoose.connect(
+  "mongodb://root:teama@mongo:27017/",
+  { userNewParser: true }
+);
 
-let url = 'mongodb://owner:teama@mongo:27017/'
-let db_name = 'testdb'
-let collection_name = 'testUser'
+mongoose.set("useCreateIndex", true);
 
-console.log(url)
-console.log(db_name)
-console.log(collection_name)
+//データーベースをdb変数に代入
+const db = mongoose.connection;
 
-const option = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}
-
-MongoClient.connect(url, option, (err, client) => {//mongodb接続
-  if (err != null || client == null) {
-    console.log("<<<ログ>>>fail")
-    console.log(err)
-  } else {
-    const db = client.db(db_name)
-    let rec = { "name": "Taro", "age": 30 }
-    db.collection(collection_name).insertOne(rec, (err, res) => {//テストデーター挿入
-      if (err != null) {
-        console.log("<<<ログ>>>err: insert")
-        console.log(err)
-        client.close()
-      } else {
-        db.collection(collection_name).find({}).toArray((err, result) => {//データー取り出し
-          if (err != null) {
-            console.log("<<<ログ>>>err: select")
-            console.log(err)
-            client.close()
-          } else {
-            console.log(result)//結果出力
-            client.close()
-          }
-        })
-      }
-    })
-  }
-})
+//接続メッセージをログに出力する
+db.once("open", () => {
+  console.log("success connect");
+});
