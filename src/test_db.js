14f09f0961eb9bac6a4df123c67d0a1098c99e67
shobaseprg <pgymlsho19128@gmail.com>
2020-12-29 19:14:39 +0900

    test

---
+++ src/test_db.js
@@ -0,0 +1,43 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+let url = 'mongodb://test:pass@mongo:27017/'
+let db_name = 'testdb'
+let collection_name = 'testUser'
+
+console.log(url)
+console.log(db_name)
+console.log(collection_name)
+
+const option = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+MongoClient.connect(url, option, (err, client) => {//mongodb接続
+  if (err != null || client == null) {
+    console.log("<<<ログ>>>fail")
+    console.log(err)
+  } else {
+    const db = client.db(db_name)
+    let rec = { "name": "Taro", "age": 30 }
+    db.collection(collection_name).insertOne(rec, (err, res) => {//テストデーター挿入
+      if (err != null) {
+        console.log("<<<ログ>>>err: insert")
+        console.log(err)
+        client.close()
+      } else {
+        db.collection(collection_name).find({}).toArray((err, result) => {//データー取り出し
+          if (err != null) {
+            console.log("<<<ログ>>>err: select")
+            console.log(err)
+            client.close()
+          } else {
+            console.log(result)//結果出力
+            client.close()
+          }
+        })
+      }
+    })
+  }
+})
