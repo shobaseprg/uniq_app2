35a2af1f6bcd3fd0867b31871dfd4d3d3d101330
shobaseprg <pgymlsho19128@gmail.com>
2020-12-30 16:30:44 +0900

    jason

--- src/test_db.js
+++ src/test_db.js
@@ -1,8 +1,9 @@
 //mongooseをロード
 const mongoose = require("mongoose");
+console.dir(process.env);
 //データーベース接続を設定
 mongoose.connect(
-  "mongodb://root:teama@mongo:27017/",
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@mongo:27017/`,
   { userNewParser: true }
 );
 
@@ -14,4 +15,4 @@ const db = mongoose.connection;
 //接続メッセージをログに出力する
 db.once("open", () => {
   console.log("success connect");
-});
+})
