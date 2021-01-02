6f401986914640b68ffd5c3564d61b0983524a87
shobaseprg <pgymlsho19128@gmail.com>
2021-01-02 18:22:15 +0900

    position seeder

--- src/models/m_position.js
+++ src/models/m_position.js
@@ -3,8 +3,8 @@
 const mongoose = require("mongoose");
 
 const positionSchema = new mongoose.Schema({
-  position_x: Number,
-  position_y: Number,
+  longitude_k_x: Number,
+  latitude_i_y: Number,
   driver: { //Fatherモデルに所属//単数形
     type: mongoose.Schema.Types.ObjectId, //外部キー
     ref: "Driver" //参照モデル
--- src/seed.js
+++ src/seed.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose"); //モジュールロード
 const seedDriver = require("./models/m_driver.js");
+const position = require("./models/m_position.js");
+
 
 mongoose.connect(
   `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@mongo:27017/uniq_db`,
@@ -9,6 +11,9 @@ mongoose.set("useCreateIndex", true);
 
 mongoose.Promise = global.Promise;
 
+//===================================
+//driver
+//===================================
 const drivers = [ //シードデーター
   {
     name: "driverTaro",
@@ -38,7 +43,40 @@ drivers.forEach((c) => { //commands配列にシーダーをループで全て追
 Promise.all(commands) //全てのプロミス解決後の処理
   .then(r => {
     console.log(JSON.stringify(r));
-    mongoose.connection.close();
+  })
+  .catch(error => {
+    console.log($error);
+  });
+
+//===================================
+//position
+//===================================
+
+const positions = [ //シードデーター
+  {
+    latitude_i_y: 35.720493999999995,
+    longitude_k_x: 139.9311911
+  },
+];
+
+position.deleteMany() //既存のデータをクリア
+  .exec()
+  .then(() => {
+    console.log("Empty"); //確認用ログ
+  });
+
+let position_seeders = []; //シーダー配列用
+
+positions.forEach((c) => { //commands配列にシーダーをループで全て追加
+  position_seeders.push(position.create({
+    latitude_i_y: c.latitude_i_y,
+    longitude_k_x: c.longitude_k_x,
+  }));
+});
+
+Promise.all(positions) //全てのプロミス解決後の処理
+  .then(r => {
+    console.log(JSON.stringify(r));
   })
   .catch(error => {
     console.log($error);
