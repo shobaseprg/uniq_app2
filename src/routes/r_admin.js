80198477b2c210071973b909d5fd91d8482855a3
shobaseprg <pgymlsho19128@gmail.com>
2021-01-03 20:50:39 +0900

    display map view page

--- src/routes/r_admin.js
+++ src/routes/r_admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/displayMap', function (req, res, next) {
-  res.send('map with a resource');
+  res.render('admin/v_admin_displayMap');
 });
 
 module.exports = router;
