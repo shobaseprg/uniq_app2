6a2eb91e295b8350fa13fd1ec845cc4b1b14326f
shobaseprg <pgymlsho19128@gmail.com>
2021-01-03 21:35:43 +0900

    change db.env secret.env

--- src/routes/r_admin.js
+++ src/routes/r_admin.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/displayMap', function (req, res, next) {
-  res.render('admin/v_admin_displayMap');
+  res.render('admin/v_admin_displayMap', { googleMapUrl: process.env.GOOGLE_MAP_API_KEY });
+  console.log("【ログ】process.env.GOOGLE_MAP_API_KEY【結果➡︎】"); console.log(process.env.GOOGLE_MAP_API_KEY);
+
 });
 
 module.exports = router;
