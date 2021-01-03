1c720edef424bb707c2b9ff720428c1397e07981
shobaseprg <pgymlsho19128@gmail.com>
2021-01-04 01:14:41 +0900

    api retrun

--- src/controllers/c_admin.js
+++ src/controllers/c_admin.js
@@ -4,15 +4,24 @@ const Position = require("../models/m_position.js");
 const Driver = require("../models/m_driver.js");
 
 const displayMap = (req, res, next) => {
-  Driver.find().populate("position").exec((error, data) => {
-    console.log(data);
-  })
   res.render('admin/v_admin_displayMap');
+}
+
+const returnCarPositions = (req, res, next) => {
+  const ppap = [
+    { object: 'pen', order: 1 },
+    { object: 'pineapple', order: 2 },
+    { object: 'apple', order: 3 },
+    { object: 'pen', order: 4 }
+  ];
+  res.json(ppap);
+  // return new promise((resolve) => {
+  //   resolve();
+  // })
 }
 
 const savePosition = (req, res, next) => {
   Driver.findOne({ name: "driverTaro" }).populate("position").exec((error, data) => {
-    console.log(req.query.latitude_i_y);
     Position.findOneAndUpdate(
       { _id: data.position._id },
       {
@@ -26,4 +35,4 @@ const savePosition = (req, res, next) => {
   res.redirect("/getPosition");
 }
 
-module.exports = { displayMap, savePosition };
+module.exports = { displayMap, savePosition, returnCarPositions };
--- src/routes/r_admin.js
+++ src/routes/r_admin.js
@@ -4,5 +4,6 @@ const c_admin = require("../controllers/c_admin");
 
 /* GET users listing. */
 router.get('/displayMap', c_admin.displayMap);
+router.get('/getCarPositions', c_admin.returnCarPositions);
 
 module.exports = router;
